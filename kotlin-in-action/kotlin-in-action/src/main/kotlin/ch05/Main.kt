1390b24eb640ce5263b67916963fede618c0beff
bong6981 <kbk5190@naver.com>
2022-11-20 18:00:55 +0900

    [kotlin-in-action] ch5: ~5.3.1 시퀀스 연산 실행: 중간 연산 최종 연산

--- kotlin-in-action/kotlin-in-action/src/main/kotlin/ch05/Main.kt
+++ kotlin-in-action/kotlin-in-action/src/main/kotlin/ch05/Main.kt
@@ -84,6 +84,14 @@ fun main(args: Array<String>) {
         .map(Person::name)
         .filter { it.startsWith("A") }
         .toList()
+
+    listOf(1, 2, 3, 4).asSequence()
+        .map { print("map($it) "); it * it }
+        .filter { print("filter ($it) "); it % 2 == 0 }
+        .toList() // map(1) filter (1) map(2) filter (4) map(3) filter (9) map(4) filter (16)
+
+    println(listOf(1, 2, 3, 4).asSequence()
+        .map { it * it }.find{ it > 3}) // 4
 }
 
 
